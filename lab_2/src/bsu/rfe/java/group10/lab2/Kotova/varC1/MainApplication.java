792df8b6725c4188daed378aa7a63604d8680deb
KseniaKotova <ksyu.kototva.79@gmail.com>
2021-11-25 22:59:24 +0300

    Add lab2 files

---
+++ lab_2/src/bsu/rfe/java/group10/lab2/Kotova/varC1/MainApplication.java
@@ -0,0 +1,11 @@
+package bsu.rfe.java.group10.lab2.Kotova.varC1;
+
+import javax.swing.JFrame;
+
+public class MainApplication {
+    public static void main(String [] args){
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
